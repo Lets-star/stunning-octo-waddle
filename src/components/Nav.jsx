8531df9298ae6cb50cbe3f109ef9dcf205197d05
Artemy <bloody_92@tut.by>
2021-04-18 21:58:03 +0200

    Lesson 14 Css Styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,34 @@
 import React from "react";
+import classes from './Nav.module.css';
+
+// let sample = {
+//   'nav' : 'navbar__4gyaUj'
+//   'item' : 'item__765fFh'
+// }
+
+// let c1 ='item'
+// let c2 = 'active'
+// 'item active'
+// let classes =c1 +' 'c2
+
+let activeItem = `${classes.item} ${classes.active}`
 
 const Nav = () => {
   return (
-    <nav className="nav">
-      <div>
+    <nav className={classes.nav}>
+      <div className = {activeItem}>
         <a>Profile</a>
       </div>
-      <div>
+      <div className = {classes.item}>
         <a>Messages</a>
       </div>
-      <div>
+      <div className = {classes.item}>
         <a>News</a>
       </div>
-      <div>
+      <div className = {classes.item}>
         <a>Music</a>
       </div>
-      <div>
+      <div className = {classes.item}>
         <a>Settings</a>
       </div>
     </nav>
