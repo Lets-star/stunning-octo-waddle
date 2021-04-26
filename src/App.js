14d03bcd79554c7b7f37936f5917c205adaad113
Artemy <bloody_92@tut.by>
2021-04-26 22:11:48 +0200

    19 lesson routing

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from 'react-router-dom';
+import Profile from "./components/Profile/Profile";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 
 const App = () => {
     return (
-
-        <div className="app-wrapper">
-            <Header/>
-            <Nav/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
-                {/* <Profile/>*/}
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Nav/>
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
             </div>
-        </div>
-
+        </BrowserRouter>
     );
 };
 
