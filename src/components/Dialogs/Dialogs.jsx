0f12bfea3de074da590d0ce1b320547b8ed9f0f7
Artemy <bloody_92@tut.by>
2021-05-02 12:38:07 +0200

    20 lesson NavLink

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,22 @@
-import  React from 'react';
+import React from 'react';
 import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
         <div>
-            Dialogs
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog+' '+ s.active}>Diana</div>
+                    <div className={s.dialog}>Sveta</div>
+                    <div className={s.dialog}>Masha</div>
+                    <div className={s.dialog}>Isvan</div>
+                </div>
+                <div classname={s.messages}>
+                    <div className={s.message}> Hi< /div>
+                    <div className={s.message}>Hi hi</div>
+                    <div className={s.message}>Yo</div>
+                </div>
+            </div>
         </div>
     )
 }
